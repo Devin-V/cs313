520364fe8bf747ee2fe1632b36e27f5232b5d7df
unknown <j.devin.vial@gmail.com>
2019-01-25 14:55:18 -0700

    merging folder

---
+++ phpFunctions.php
@@ -0,0 +1,7 @@
+<?php
+
+function push(item){
+    array_push($list, item);
+}
+
+?>
---
+++ week02.js
@@ -0,0 +1,50 @@
+function addItem(item){
+    $.ajax({
+        url : "phpFunctions.php",
+        type : "POST",
+        data : {functionname : 'push', arguments: [item]}
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+/*var list = [];
+
+function addItem(item){
+    list.push(item);
+    console.log(list);
+    console.log(list.length);
+    document.cookie = list;
+}
+
+function deleteCookies(){
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    console.log("deleting");
+}
+
+function goBack(){
+    window.history.back();
+}
+
+function onLoad(){
+    var x = document.cookie;
+    console.log("here I am");
+    console.log(x.length);
+    console.log(x);
+    var list2 = x.split(",");
+    for (var i = 0; i < list2.length; ++i) {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(list2[i]);
+        node.appendChild(textnode);
+        document.getElementById("container").appendChild(node);
+        console.log(list2[i]);
+    }
+}*/
