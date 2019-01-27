8d4680b92c28648d68311cbb4c8b036651774d77
unknown <j.devin.vial@gmail.com>
2019-01-26 18:08:48 -0700

    checkboxes

--- phpFunctions.php
+++ phpFunctions.php
@@ -20,6 +20,7 @@
         </ol>
         <form name="form" action="" method="get">
             <input type="number" name="item to delete" id="deleteNum" min="1" max="8">
+            <input type="submit" value="Delete Item">
         </form>
             <?php
                 $numz = $_GET['item to delete'];
