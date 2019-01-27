4785dd5e60a50547b632409b330a202f3af16d85
unknown <j.devin.vial@gmail.com>
2019-01-26 17:44:39 -0700

    checkboxes

--- phpFunctions.php
+++ phpFunctions.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html>
 <?php
     $list = $_POST["list"];
 ?>
+
+<!DOCTYPE html>
 <html>
     <head>
         <title>Cart</title>
@@ -9,10 +10,10 @@
     <body>
         <h1>Contents of Cart</h1>
         <ul>
-            <?
+            <?php
             foreach ($list as $item)
             {
-                $items = $list);
+                $items = $list;
                 echo "<li><p>$items</p></li>";
             }
             ?>
