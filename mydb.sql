651ab28f46f24ffa47678b23866704384824964f
unknown <j.devin.vial@gmail.com>
2019-02-02 13:43:32 -0700

    database

---
+++ mydb.sql
@@ -0,0 +1,27 @@
+CREATE TABLE public.timed(
+    id SERIAL NOT NULL PRIMARY KEY,
+    hour INT NOT NULL,
+    min INT NOT NULL,
+    day INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL
+);
+
+CREATE TABLE public.description(
+    id SERIAL NOT NULL PRIMARY KEY,
+    color VARCHAR(100),
+    model VARCHAR(100)
+);
+
+CREATE TABLE public.item(
+    id SERIAL NOT NULL PRIMARY KEY,
+    price INT NOT NULL,
+    description_id INT NOT NULL REFERENCES public.description(id)
+);
+
+CREATE TABLE public.saledata(
+    id SERIAL NOT NULL PRIMARY KEY,
+    person VARCHAR(100) NOT NULL,
+    item INT NOT NULL REFERENCES public.item(id),
+    timed INT NOT NULL REFERENCES public.timed(id)
+);
