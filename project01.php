8ab994b5d59cbd13440ba9305cf6894cb7286599
unknown <j.devin.vial@gmail.com>
2019-02-07 17:49:52 -0700

    testing

--- project01.php
+++ project01.php
@@ -32,7 +32,7 @@ catch (PDOException $ex)
 <html>
     <head>
     <title>project01</title>
-    <script> function reload(){location.reload();}</script>
+    <script> function reload(){window.location.reload();}</script>
     </head>
     <body>
         <form name="form" method="POST" action="project01.php">
