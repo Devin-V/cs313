930406d1fdd740cea18163806046d8df05527585
unknown <j.devin.vial@gmail.com>
2019-02-15 15:42:50 -0700

    insert data

--- project01-insert.php
+++ project01-insert.php
@@ -8,18 +8,25 @@
     $itemSold = $_POST['item'];
     $employeeSale = $_POST['employee'];
 
-    echo "Variables Set";
+    echo "Variables Set <br>";
+    echo "timestamp=$timestamp\n";
+    echo "purchaseType=$purchaseType\n";
+    echo "customerType=$customerType\n";
+    echo "onSale=$onSale\n";
+    echo "itemSold=$itemSold\n";
+    echo "employeeSale=$employeeSale\n";
 
 // Connect to DB
     require("dbConnect.php");
     $db = get_db();
-    echo "Connected to Database";
+    echo "Connected to Database\n";
 
 // Add to Sales Table
     try
     {
         $query = 'INSERT INTO sales(timeofpurchase, typeofpurchase, typeofcustomer, onsale, item, employee) VALUES(:timestamp, :purchaseType, :customerType, :onSale, :itemSold, :employeeSale)';
         $statement = $db->prepare($query);
+        echo "Query Setup\n";
 
         $statement->bindValue(':timestamp', $timestamp);
         $statement->bindValue(':purchaseType', $purchaseType);
@@ -29,11 +36,11 @@
         $statement->bindValue(':employeeSale', $employeeSale);
 
         $statment->execute();
-        echo "excecuted";
+        echo "excecuted\n";
     }
     catch (Exception $ex)
     {
-        echo "Error with DB. Detials: $ex";
+        echo "Error with DB. Details: $ex";
         die();
     }
 
