b48af28623da96abc4289c5b4b05ce2daf122c5b
unknown <j.devin.vial@gmail.com>
2019-02-15 15:04:07 -0700

    insert data

--- mydb.sql
+++ mydb.sql
@@ -46,6 +46,7 @@ CREATE TABLE public.sales(
 
 CREATE TABLE public.items(
     id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR (100),
     price INT NOT NULL,
     stock INT NOT NULL,
     typeofitem VARCHAR(100)
@@ -54,7 +55,7 @@ CREATE TABLE public.items(
 CREATE TABLE public.adverts(
     id SERIAL NOT NULL PRIMARY KEY,
     typeofitem VARCHAR(100),
-    price INT NOT NULL
+    price FLOAT NOT NULL
 );
 
 CREATE TABLE public.employee(
@@ -63,20 +64,32 @@ CREATE TABLE public.employee(
     title VARCHAR(100),
     numsales INT NOT NULL,
     numloyalty INT NOT NULL,
-    phonenumber INT NOT NULL,
-    numprojects INT NOT NULL,
-    custscore INT NOT NULL
+    phonenumber VARCHAR(100),
+    numprojects INT NOT NULL
 );
 
 /*------------------------------------------------------------*/
 
-INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects, custscore) VALUES (1, 'DEVIN','cashier', 23, 2, 1234567891, 25, 8.5);
-INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects, custscore) VALUES (2, 'dennis', 'cashier', 3, 23, 1234567890, 5, 2.5);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (1, 'devin','cashier', 23, 2, '2083214567', 25);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (2, 'dennis', 'cashier', 73, 23, '1234567890', 5);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (3, 'john', 'cashier', 100, 50, '2083215567', 50);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (4, 'doug', 'cashier', 56, 20, '2083214767', 20);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (5, 'william', 'assistant manager', 20, 30, '2083204567', 100);
+INSERT INTO employee (id, name, title, numsales, numloyalty, phonenumber, numprojects) VALUES (6, 'sharon', 'manager', 30, 45, '2083219567', 150);
 INSERT INTO adverts (id, typeofitem, price) VALUES (1, 'food', 500);
 INSERT INTO adverts (id, typeofitem, price) VALUES (2, 'electronics', 5000);
-INSERT INTO items (id, price, stock, typeofitem) VALUES (1, 25.99, 30, 'food');
-INSERT INTO items (id, price, stock, typeofitem) VALUES (2, 109.50, 25, 'electronics');
-INSERT INTO sales (id, timeofpurchase, typeofpurchase, typeofcustomer, onsale, item, employee) VALUES (1, '2018-01-08 04:05:06', 'visa', 'standard', 'true', 1, 1);
-INSERT INTO sales (id, timeofpurchase, typeofpurchase, typeofcustomer, onsale, item, employee) VALUES (2, '2018-01-08 04:06:12', 'cash', 'loyalty', 'false', 2, 2);
+INSERT INTO adverts (id, typeofitem, price) VALUES (3, 'sports', 2000);
+INSERT INTO adverts (id, typeofitem, price) VALUES (4, 'beauty', 500);
+INSERT INTO adverts (id, typeofitem, price) VALUES (5, 'toys', 500);
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (1, 'roast', 25.99, 130, 'food');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (2, 'milk', 2.25, 250, 'food');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (3, 'tv', 109.50, 125, 'electronics');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (4, 'computer', 500.50, 312, 'electronics');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (5, 'basketball', 50.99, 100, 'sports');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (6, 'hockey stick', 50.00, 100, 'sports');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (7, 'makeup', 4.75, 800, 'beauty');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (8, 'hair dryer', 30.25, 105, 'beauty');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (9, 'action figure', 27.00, 712, 'toys');
+INSERT INTO items (id, name, price, stock, typeofitem) VALUES (10, 'baby blocks', 20.00, 101, 'toys');
 
 /*************************************************************/
--- project01.php
+++ project01.php
@@ -13,7 +13,41 @@
         </div>
         <div class="mostPage">
         <h1>Input A Sale</h1>
-        <p>This is where a sale will be made and added to the database</p>
+        <form method="POST" action="project01.php">
+            <input type="datetime-local" name="timeofpurchase">
+            <select name="typeofpurchase">
+                <option value="visa">Visa</option>
+                <option value="amx">American Express</option>
+                <option value="mastercard">Mastercard</option>
+                <option value="cash">Cash</option>
+            </select>
+            <select name="typeofcustomer">
+                <option value="standard">Standard</option>
+                <option value="loyalty">Loyalty</option>
+            </select>
+            <input type="radio" name="onsale" value="true" checked>Item was not on sale<br>
+            <input type="radio" name="onsale" value="false">Item is on sale<br>
+            <select name="item">
+                <option value="roast">Roast</option>
+                <option value="milk">Milk</option>
+                <option value="tv">Television</option>
+                <option value="computer">Computer</option>
+                <option value="basketball">Basketball</option>
+                <option value="hockey stick">Hockey Stick</option>
+                <option value="makeup">makeup</option>
+                <option value="hair dryer">Hair Dryer</option>
+                <option value="action figure">Action Figure</option>
+                <option value="baby blocks">Baby Blocks</option>
+            </select>
+            <select name="employee">
+                <option value="devin">Devin</option>
+                <option value="dennis">Dennis</option>
+                <option value="john">John</option>
+                <option value="doug">Doug</option>
+                <option value="william">William</option>
+                <option value="sharon">Sharon</option>
+            </select>
+            <input type="submit" value="Submit Order">
         </div>
     </body>
 </html>
